fe003d3ae6212295faf3e2f8f72006245c2f2b11
alicinia <alicinia97@gmail.com>
2023-09-25 17:19:56 +0700

    task display changed

--- src/components/AddTaskField.tsx
+++ src/components/AddTaskField.tsx
@@ -1,10 +1,13 @@
+import * as React from 'react';
 import { TextField, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
 export default function AddTaskField() {
+	const [taskText, setTaskText] = React.useState('');
+
 	return (
 		<Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-			<AddIcon onClick={() => { }} sx={{ color: 'action.active', mr: 1, my: 0.5 }} fontSize="medium" />
+			<AddIcon onClick={() => { console.log(taskText)}} sx={{ color: 'action.active', mr: 1, my: 0.5 }} fontSize="medium" />
 			{/* //TODO: add click hendler 
           	*/}
 			<TextField
@@ -12,7 +15,10 @@ export default function AddTaskField() {
 				label="What needs to be done?"
 				variant="standard"
 				autoFocus={true}
-				fullWidth={true} />
+				fullWidth={true}
+				defaultValue={taskText} 
+				onChange={(e)=> setTaskText(e.target.value)}
+				/>
 		</Box>
 	);
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,16 @@
+import * as React from 'react';
 import Task from './Task';
 import { List } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
 
-const task1 = { title: 'task1', completed: true, id: uuidv4() };
-const task2 = { title: 'task2', completed: false, id: uuidv4() };
+const taskList = [{ title: 'task1', completed: true, id: uuidv4() },{ title: 'task2', completed: false, id: uuidv4() }];
 
 export default function TodoList() {
+	const [tasks, setTasks] = React.useState(taskList);
 	return (
 		<List sx={{ py: 2 }}>
-			<Task task={task1} />
-			<Task task={task2} />
+			<Task task={tasks[0]} />
+			<Task task={tasks[1]} />
 		</List>
 	);
 }
